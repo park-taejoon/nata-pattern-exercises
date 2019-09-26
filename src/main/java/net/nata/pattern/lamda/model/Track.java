18d4ca7757e888c52117eb7fef189270fae7791e
tjpark <git.adlib@gmail.com>
2019-09-26 18:09:00 +0900

    f

--- src/main/java/net/nata/pattern/lamda/model/Track.java
+++ src/main/java/net/nata/pattern/lamda/model/Track.java
@@ -1,8 +1,30 @@
 package net.nata.pattern.lamda.model;
 
-import lombok.Data;
-
-@Data
 public class Track {
-	String track;
+	private final String name;
+    private final int length;
+
+    public Track(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the length of the track in milliseconds.
+     */
+    public int getLength() {
+        return length;
+    }
+
+    public Track copy() {
+        return new Track(name, length);
+    }
+
 }
--- src/main/java/net/nata/pattern/lamda/step1/LamdaExample2.java
+++ src/main/java/net/nata/pattern/lamda/step1/LamdaExample2.java
@@ -1,9 +1,14 @@
 package net.nata.pattern.lamda.step1;
 
+import java.text.SimpleDateFormat;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import javax.annotation.PostConstruct;
+import javax.swing.JButton;
+import javax.swing.text.DateFormatter;
 
 import org.springframework.stereotype.Component;
 
@@ -23,6 +28,22 @@ public class LamdaExample2 {
 		
 		CustomFunction func = (x, y) -> String.valueOf(x*y);
 		log.info("result2 : {}",func.init(10L, 3));
+		
+		Function<Long, Boolean> fun2 = (x) -> x == 3L;
+	//	log.info("result3 : {}",fun2.apply( 3L));
+		
+		CustomFunction func3 = (x, y) -> 
+										String.valueOf(x*y);
+		
+		ThreadLocal<Integer> threadLocal =ThreadLocal.withInitial(() -> 1);//supplier 타입이므로.
+		log.info("result3 : {}",threadLocal.get());
+		
+		ThreadLocal<DateFormatter> threadLocal2 =ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
+		log.info("result4 : {}",threadLocal2.get().toString());
+		
+		
+		JButton button = new JButton();
+		button.addActionListener(event -> log.info("task : {}",event.getActionCommand()));
 		
 		//BinaryOperator add = (x,y) -> x+y;//타입 명시가 되지 않아 컴파일 되지 않음
 		log.info("---------------LadaExample2 E N D----------------");
